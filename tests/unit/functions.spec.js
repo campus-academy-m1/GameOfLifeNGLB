225cb4dd3db14326ec22b5ec278eca7d5ef077bc
Nicolas Gerard <nicolas.gerard@ynov.com>
2021-04-06 17:22:05 +0200

    80% coverage

--- tests/unit/functions.spec.js
+++ tests/unit/functions.spec.js
@@ -8,25 +8,25 @@ describe("App.vue", () => {
     var color = wrapper.vm.getRndColor();
     expect(color).to.include("#");
   });
-  //   it("test fonction getRandomInteger", () => {
-  //     const wrapper = mount(App);
-  //     var random = wrapper.vm.getRandomInteger("1", "1");
-  //     expect(random).to.include(1);
-  //   });
-  //   it("test fonction generateGrid", () => {
-  //     const wrapper = mount(App);
-  //     var random = wrapper.vm.generateGrid();
-  //   });
-  //   it("test fonction generateGrid", () => {
-  //     const wrapper = mount(App);
-  //     var random = wrapper.vm.intervalFunction();
-  //   });
-  //   it("test fonction play", () => {
-  //     const wrapper = mount(App);
-  //     var random = wrapper.vm.play();
-  //   });
-  //   it("test fonction clear", () => {
-  //     const wrapper = mount(App);
-  //     var random = wrapper.vm.clear();
-  //   });
+  it("test fonction getRandomInteger", () => {
+    const wrapper = mount(functions);
+    var random = wrapper.vm.getRandomInteger("1", "1");
+    expect(random).to.include(1);
+  });
+  it("test fonction generateGrid", () => {
+    const wrapper = mount(functions);
+    var random = wrapper.vm.generateGrid();
+  });
+  it("test fonction generateGrid", () => {
+    const wrapper = mount(functions);
+    var random = wrapper.vm.intervalFunction();
+  });
+  it("test fonction play", () => {
+    const wrapper = mount(functions);
+    var random = wrapper.vm.play();
+  });
+  it("test fonction clear", () => {
+    const wrapper = mount(functions);
+    var random = wrapper.vm.clear();
+  });
 });
